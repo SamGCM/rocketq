a5c4f74e3a60e788d730878ffe10659b49f50205
Samuel Cruz <samuelcruz052@gmail.com>
2021-06-26 02:59:36 -0300

    criação do db

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,22 +5,71 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
+        while(isRoom){
 
-        for(var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+            /* GERA O NÚMERO DA SALA */
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+            /* VERIFICA SE O NÚMERO JÁ EXISTE */
+
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomExistId == roomId )
+
+            if(! isRoom){
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VAlUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+        
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VAlUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req,res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions
+
+        if(questions.length == 0){
+            if(questionsRead.length == 0){
+                isNoQuestions = true
+            }
+        }
+
+
+        res.render('room', {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+    },
+
+    async enter(req, res){
+        const db = await Database()
+
+        const roomId = req.body.roomId
+        let isRoom = true
+        while(isRoom){
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomExistId == roomId )
+            
+            if(!isRoom){
+                res.render(`/room-not-exist`)
+            }else{
+                res.redirect(`/room/${roomId}`)
+            }
+        }
+        await db.close()
+
+        
+        
     }
 }
