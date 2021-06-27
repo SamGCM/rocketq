3bd9f571494cf84210629267375d2114a0ad956d
Samuel Cruz <samuelcruz052@gmail.com>
2021-06-27 20:10:15 -0300

    syle: add toggle funcional com local storage

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,10 +1,95 @@
-let data = new Date()
-const hour = data.getHours()
-
 
 const toggle = document.querySelector('.toggle')
 
 const mode = document.querySelector('.circle')
+
+
+// pegamos o valor no localStorage
+const nightModeStorage = localStorage.getItem('gmtNightMode')
+
+// caso tenha o valor no localStorage
+if (nightModeStorage) {
+  // ativa o night mode
+    mode.classList.add('night-mode')
+
+    
+
+
+
+    const logo = document.querySelector('.night-logo')
+
+    const balls = document.querySelectorAll('.night-ball')
+
+    const bgSvg = document.querySelector('#bg')
+
+    const bgBox = document.querySelectorAll('.night-bg-box')
+
+    const bgBoxDesactive = document.querySelectorAll('.night-bg-desactive')
+
+    const iconUser = document.querySelectorAll('.night-icon-user')
+
+    const iconCopy = document.querySelector('.night-icon-copy')
+
+    const user = document.querySelectorAll('.night-user')
+
+    const noQuestion = document.querySelector('.svg-no-question')
+
+    const body = document.querySelector('.night-bg')
+    const buttons = document.querySelectorAll('.night-button')
+    const word = document.querySelectorAll('.night-word')
+
+
+
+
+
+
+    bgSvg == null ? '' : bgSvg.style.setProperty('background', "url('../images/img-bg-night.svg') no-repeat top/contain")
+    bgSvg == null ? '' : bgSvg.style.setProperty('background-position-x', "-10rem")
+
+    logo == null ? '' : logo.setAttribute('src', '/images/Logo white.svg')
+
+    noQuestion == null ? '' : noQuestion.setAttribute('src', '/images/Ilustração night.svg')
+
+    iconCopy == null ? '' : iconCopy.setAttribute('src', '/images/copy white.svg')
+
+    user == null ? '' : user.forEach(icon => {
+        icon.style.setProperty('background', '#8257E6')
+    })
+
+
+
+    balls.forEach(ball => {
+        ball.style.setProperty('background', '#8257E6')
+    })
+
+    bgBox.forEach(background => {
+        background.style.setProperty('background', 'var(--bg-gradient-night)')
+    })
+
+    bgBoxDesactive.forEach(background => {
+        background.style.setProperty('background', 'var(--bg-gradient-night-desactive)')
+    })
+
+
+    body.style.setProperty('background', 'var(--background-night)')
+
+
+    iconUser.forEach( icon => {
+        icon.setAttribute('src', '/images/user 1.svg')
+    })
+
+
+    buttons.forEach( button => {
+        button.style.setProperty('background', 'var(--bg-gradient-night)')
+        button.style.setProperty('border', '0.2rem solid var(--bg-gradient-night)')
+    })
+
+    word.forEach(word => {
+        word.style.setProperty('color', 'var(--white)')
+    })
+}
+
+
 
 toggle.addEventListener('click', () => {
 
@@ -34,7 +119,7 @@ toggle.addEventListener('click', () => {
         
         const buttons = document.querySelectorAll('.night-button')
 
-        const btnOutilined = document.querySelectorAll('.outilined')
+        const btnOutilined = document.querySelectorAll('.outlined')
 
         const word = document.querySelectorAll('.night-word')
 
@@ -88,6 +173,13 @@ toggle.addEventListener('click', () => {
             button.style.setProperty('color', 'var(--white)')
         })
 
+        btnOutilined.forEach(button => {
+            button.style.setProperty('background', 'var(--white)')
+            button.style.setProperty('color', 'var(--black)')
+        })
+
+        localStorage.removeItem('gmtNightMode')
+
         
 
         
@@ -99,7 +191,9 @@ toggle.addEventListener('click', () => {
 
     } else{
         mode.classList.add('night-mode')
-        mode.classList.remove('light-mode')      
+        mode.classList.remove('light-mode')
+        
+        
 
         const logo = document.querySelector('.night-logo')
 
@@ -172,5 +266,8 @@ toggle.addEventListener('click', () => {
         word.forEach(word => {
             word.style.setProperty('color', 'var(--white)')
         })
+
+        localStorage.setItem('gmtNightMode', true)
+        return
     }
 })
